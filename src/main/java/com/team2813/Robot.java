5001e7bd18b47ad18e29ec52438cc78fd12aef66
codingman106 <jazl.ihsan@gmail.com>
2025-01-20 17:40:11 -0800

    Fixed slight errors

--- src/main/java/com/team2813/Robot.java
+++ src/main/java/com/team2813/Robot.java
@@ -4,6 +4,10 @@
 
 package com.team2813;
 
+import java.io.IOException;
+
+import org.json.simple.parser.ParseException;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -13,7 +17,7 @@ public class Robot extends TimedRobot {
 
   private final RobotContainer m_robotContainer;
 
-  public Robot() {
+  public Robot() throws IOException, ParseException {
     m_robotContainer = new RobotContainer();
   }
 
