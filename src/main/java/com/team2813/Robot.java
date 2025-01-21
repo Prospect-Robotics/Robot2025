42ad18cb54b0ff7015c04509b133e6e06cb0b74c
cuttestkittensrule <mangoiscute95@gmail.com>
2025-01-21 10:00:34 -0800

    Revert "10 setup pathplanner config" (#19)

--- src/main/java/com/team2813/Robot.java
+++ src/main/java/com/team2813/Robot.java
@@ -4,10 +4,6 @@
 
 package com.team2813;
 
-import java.io.IOException;
-
-import org.json.simple.parser.ParseException;
-
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -17,7 +13,7 @@ public class Robot extends TimedRobot {
 
   private final RobotContainer m_robotContainer;
 
-  public Robot() throws IOException, ParseException {
+  public Robot() {
     m_robotContainer = new RobotContainer();
   }
 
