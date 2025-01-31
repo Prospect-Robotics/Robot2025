e01d200066e4b2a3166e0a5c3aefc6fb5da30534
Tom Joseph <tj67542007@gmail.com>
2025-01-30 20:14:42 -0800

    Climb Code
    Started climb code

--- src/main/java/com/team2813/subsystems/Climb.java
+++ src/main/java/com/team2813/subsystems/Climb.java
@@ -1,5 +1,63 @@
 package com.team2813.subsystems;
+// import static com.team2813.Constants.CLIMBER; 
 
-public class Climb {
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.team2813.lib2813.control.ControlMode;
+import com.team2813.lib2813.control.InvertType;
+import com.team2813.lib2813.control.PIDMotor;
+import com.team2813.lib2813.control.motors.TalonFXWrapper;
+import com.team2813.lib2813.util.ConfigUtils;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+public class Climb extends SubsystemBase{ 
+
+    private final PIDMotor ClimbMotor1; 
+    private final PIDMotor ClimbMotor2;
+
+    public Climber() {
+        TalonFXWrapper ClimbMotor1 = new TalonFXWrapper(null, InvertType.CLOCKWISE);
+		ClimbMotor1.motor().setNeutralMode(NeutralModeValue.Brake);
+		TalonFXConfigurator cnf1 = ClimbMotor1.motor().getConfigurator();
+		ConfigUtils.phoenix6Config(
+				() -> cnf.apply(
+					new SoftwareLimitSwitchConfigs()
+					.withForwardSoftLimitEnable(true)
+					.withForwardSoftLimitThreshold(null)
+					.withReverseSoftLimitEnable(true)
+					.withReverseSoftLimitThreshold(null)
+				)
+		); 
+        this.ClimbMotor1 = ClimbMotor1; 
+        TalonFXWrapper ClimbMotor2 = new TalonFXWrapper(null, InvertType.CLOCKWISE);
+		ClimbMotor2.motor().setNeutralMode(NeutralModeValue.Brake);
+		TalonFXConfigurator cnf2 = ClimbMotor2.motor().getConfigurator();
+		ConfigUtils.phoenix6Config(
+				() -> cnf.apply(
+					new SoftwareLimitSwitchConfigs()
+					.withForwardSoftLimitEnable(true)
+					.withForwardSoftLimitThreshold(null)
+					.withReverseSoftLimitEnable(true)
+					.withReverseSoftLimitThreshold(null)
+				)
+		); 
+        this.ClimbMotor2 = ClimbMotor2; 
+    }
+    public void extend() {
+		ClimbMotor1.set(ControlMode.DUTY_CYCLE, null);
+        ClimbMotor2.set(ControlMode.DUTY_CYCLE, null);
+	}
+
+	public void retract() {
+		ClimbMotor1.set(ControlMode.DUTY_CYCLE, null);
+        ClimbMotor2.set(ControlMode.DUTY_CYCLE, null);
+	}
+
+	public void stop() {
+		ClimbMotor1.set(ControlMode.DUTY_CYCLE, null);
+        ClimbMotor2.set(ControlMode.DUTY_CYCLE, null);
+	}
+   
 }
