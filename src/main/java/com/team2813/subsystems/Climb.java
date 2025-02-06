55fae50d00a72384dd57bd0cc403231712eec12a
Tom Joseph <tj67542007@gmail.com>
2025-02-05 22:49:56 -0800

    Changes null values to 0

--- src/main/java/com/team2813/subsystems/Climb.java
+++ src/main/java/com/team2813/subsystems/Climb.java
@@ -18,46 +18,46 @@ public class Climb extends SubsystemBase{
     private final PIDMotor ClimbMotor2;
 
     public Climber() {
-        TalonFXWrapper ClimbMotor1 = new TalonFXWrapper(null, InvertType.CLOCKWISE);
+        TalonFXWrapper ClimbMotor1 = new TalonFXWrapper(0, InvertType.CLOCKWISE);
 		ClimbMotor1.motor().setNeutralMode(NeutralModeValue.Brake);
 		TalonFXConfigurator cnf1 = ClimbMotor1.motor().getConfigurator();
 		ConfigUtils.phoenix6Config(
 				() -> cnf.apply(
 					new SoftwareLimitSwitchConfigs()
 					.withForwardSoftLimitEnable(true)
-					.withForwardSoftLimitThreshold(null)
+					.withForwardSoftLimitThreshold(0)
 					.withReverseSoftLimitEnable(true)
-					.withReverseSoftLimitThreshold(null)
+					.withReverseSoftLimitThreshold(0)
 				)
 		); 
         this.ClimbMotor1 = ClimbMotor1; 
-        TalonFXWrapper ClimbMotor2 = new TalonFXWrapper(null, InvertType.CLOCKWISE);
+        TalonFXWrapper ClimbMotor2 = new TalonFXWrapper(0, InvertType.CLOCKWISE);
 		ClimbMotor2.motor().setNeutralMode(NeutralModeValue.Brake);
 		TalonFXConfigurator cnf2 = ClimbMotor2.motor().getConfigurator();
 		ConfigUtils.phoenix6Config(
 				() -> cnf.apply(
 					new SoftwareLimitSwitchConfigs()
 					.withForwardSoftLimitEnable(true)
-					.withForwardSoftLimitThreshold(null)
+					.withForwardSoftLimitThreshold(0)
 					.withReverseSoftLimitEnable(true)
-					.withReverseSoftLimitThreshold(null)
+					.withReverseSoftLimitThreshold(0)
 				)
 		); 
         this.ClimbMotor2 = ClimbMotor2; 
     }
     public void extend() {
-		ClimbMotor1.set(ControlMode.DUTY_CYCLE, null);
-        ClimbMotor2.set(ControlMode.DUTY_CYCLE, null);
+		ClimbMotor1.set(ControlMode.DUTY_CYCLE, 0);
+        ClimbMotor2.set(ControlMode.DUTY_CYCLE, 0);
 	}
 
 	public void retract() {
-		ClimbMotor1.set(ControlMode.DUTY_CYCLE, null);
-        ClimbMotor2.set(ControlMode.DUTY_CYCLE, null);
+		ClimbMotor1.set(ControlMode.DUTY_CYCLE, 0);
+        ClimbMotor2.set(ControlMode.DUTY_CYCLE, 0);
 	}
 
 	public void stop() {
-		ClimbMotor1.set(ControlMode.DUTY_CYCLE, null);
-        ClimbMotor2.set(ControlMode.DUTY_CYCLE, null);
+		ClimbMotor1.set(ControlMode.DUTY_CYCLE, 0);
+        ClimbMotor2.set(ControlMode.DUTY_CYCLE, 0);
 	}
    
 }
