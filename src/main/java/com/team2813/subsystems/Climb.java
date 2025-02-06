2a09ea4810c4d6495ecfe8ce42bbd3f2ac8c09d7
Tom Joseph <tj67542007@gmail.com>
2025-02-06 14:24:20 -0800

    Updated climb
    Updated climb code from feedback

--- src/main/java/com/team2813/subsystems/Climb.java
+++ src/main/java/com/team2813/subsystems/Climb.java
@@ -1,5 +1,5 @@
 package com.team2813.subsystems;
-// import static com.team2813.Constants.CLIMBER; 
+import static com.team2813.Constants.CLIMB; 
 
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
@@ -15,10 +15,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Climb extends SubsystemBase{ 
 
     private final PIDMotor ClimbMotor1; 
-    private final PIDMotor ClimbMotor2;
 
-    public Climber() {
-        TalonFXWrapper ClimbMotor1 = new TalonFXWrapper(0, InvertType.CLOCKWISE);
+    public Climb() {
+        TalonFXWrapper ClimbMotor1 = new TalonFXWrapper(CLIMB_1, InvertType.CLOCKWISE);
 		ClimbMotor1.motor().setNeutralMode(NeutralModeValue.Brake);
 		TalonFXConfigurator cnf1 = ClimbMotor1.motor().getConfigurator();
 		ConfigUtils.phoenix6Config(
@@ -31,33 +30,15 @@ public class Climb extends SubsystemBase{
 				)
 		); 
         this.ClimbMotor1 = ClimbMotor1; 
-        TalonFXWrapper ClimbMotor2 = new TalonFXWrapper(0, InvertType.CLOCKWISE);
-		ClimbMotor2.motor().setNeutralMode(NeutralModeValue.Brake);
-		TalonFXConfigurator cnf2 = ClimbMotor2.motor().getConfigurator();
-		ConfigUtils.phoenix6Config(
-				() -> cnf.apply(
-					new SoftwareLimitSwitchConfigs()
-					.withForwardSoftLimitEnable(true)
-					.withForwardSoftLimitThreshold(0)
-					.withReverseSoftLimitEnable(true)
-					.withReverseSoftLimitThreshold(0)
-				)
-		); 
-        this.ClimbMotor2 = ClimbMotor2; 
+		ClimbMotor1.addFollower(CLIMB_2, InvertType.FOLLOW_MASTER);
     }
     public void extend() {
 		ClimbMotor1.set(ControlMode.DUTY_CYCLE, 0);
-        ClimbMotor2.set(ControlMode.DUTY_CYCLE, 0);
 	}
 
-	public void retract() {
-		ClimbMotor1.set(ControlMode.DUTY_CYCLE, 0);
-        ClimbMotor2.set(ControlMode.DUTY_CYCLE, 0);
-	}
 
 	public void stop() {
 		ClimbMotor1.set(ControlMode.DUTY_CYCLE, 0);
-        ClimbMotor2.set(ControlMode.DUTY_CYCLE, 0);
 	}
    
 }
