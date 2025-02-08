bd36ae4c9ab0db0bd06029ce8d2fa977e6525a6a
cuttestkittensrule <mangoiscute95@gmail.com>
2025-02-07 18:23:31 -0800

    fix indentation and compile-time errors

--- src/main/java/com/team2813/subsystems/Climb.java
+++ src/main/java/com/team2813/subsystems/Climb.java
@@ -1,5 +1,4 @@
 package com.team2813.subsystems;
-import static com.team2813.Constants.CLIMB; 
 
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
@@ -10,35 +9,37 @@ import com.team2813.lib2813.control.PIDMotor;
 import com.team2813.lib2813.control.motors.TalonFXWrapper;
 import com.team2813.lib2813.util.ConfigUtils;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-public class Climb extends SubsystemBase{ 
 
-    private final PIDMotor ClimbMotor1; 
+import static com.team2813.Constants.*;
+
+public class Climb extends SubsystemBase{
+
+    private final PIDMotor climbMotor1;
 
     public Climb() {
-        TalonFXWrapper ClimbMotor1 = new TalonFXWrapper(CLIMB_1, InvertType.CLOCKWISE);
-		ClimbMotor1.motor().setNeutralMode(NeutralModeValue.Brake);
-		TalonFXConfigurator cnf1 = ClimbMotor1.motor().getConfigurator();
-		ConfigUtils.phoenix6Config(
-				() -> cnf.apply(
-					new SoftwareLimitSwitchConfigs()
-					.withForwardSoftLimitEnable(true)
-					.withForwardSoftLimitThreshold(0)
-					.withReverseSoftLimitEnable(true)
-					.withReverseSoftLimitThreshold(0)
-				)
-		); 
-        this.ClimbMotor1 = ClimbMotor1; 
-		ClimbMotor1.addFollower(CLIMB_2, InvertType.FOLLOW_MASTER);
+        TalonFXWrapper climbMotor1 = new TalonFXWrapper(CLIMB_1, InvertType.CLOCKWISE);
+				climbMotor1.motor().setNeutralMode(NeutralModeValue.Brake);
+				TalonFXConfigurator cnf = climbMotor1.motor().getConfigurator();
+				ConfigUtils.phoenix6Config(
+						() -> cnf.apply(
+							new SoftwareLimitSwitchConfigs()
+							.withForwardSoftLimitEnable(true)
+							.withForwardSoftLimitThreshold(0)
+							.withReverseSoftLimitEnable(true)
+							.withReverseSoftLimitThreshold(0)
+						)
+				);
+				this.climbMotor1 = climbMotor1;
+				climbMotor1.addFollower(CLIMB_2, InvertType.FOLLOW_MASTER);
     }
     public void extend() {
-		ClimbMotor1.set(ControlMode.DUTY_CYCLE, 0);
+		climbMotor1.set(ControlMode.DUTY_CYCLE, 0);
 	}
 
 
 	public void stop() {
-		ClimbMotor1.set(ControlMode.DUTY_CYCLE, 0);
+		climbMotor1.set(ControlMode.DUTY_CYCLE, 0);
 	}
    
 }
