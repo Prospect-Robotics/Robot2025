aebe7025a4c8a14bf4ee477a9a29192432bf07e4
Kevin Cooney <kcooney@gmail.com>
2025-02-07 18:28:54 -0800

    Add test to verify that RobotContainer can be constructed

---
+++ src/test/java/com/team2813/RobotContainerTest.java
@@ -0,0 +1,13 @@
+package com.team2813;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class RobotContainerTest {
+    @Test
+    public void constructorDoesNotRaise() {
+        new RobotContainer();
+    }
+}
