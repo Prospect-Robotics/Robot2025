ccacea1e45700df46a60096b047a676a43737f95
Tom Joseph <tj67542007@gmail.com>
2025-02-13 21:23:33 -0800

    Operator button bindings
    first iteration of operator button bindings

--- src/main/java/com/team2813/RobotContainer.java
+++ src/main/java/com/team2813/RobotContainer.java
@@ -13,25 +13,62 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+
+import com.team2813.subsystems.Climb;
 import com.team2813.subsystems.Drive;
+import com.team2813.subsystems.Elevator;
+import com.team2813.subsystems.Intake;
+import com.team2813.subsystems.IntakePivot;
 
 public class RobotContainer {
-
-  
+  private final Climb climb = new Climb();
+  private final Intake intake = new Intake();
+  private final IntakePivot intakepivot = new IntakePivot();
+  private final Elevator elevator = new Elevator(); 
   private final SendableChooser<Command> autoChooser;
   private final Drive drive = new Drive();
 
   public RobotContainer() {
     // Build an auto chooser. This will use Commands.none() as the default option.
     autoChooser = AutoBuilder.buildAutoChooser();
-
+     
     SmartDashboard.putData("Auto Chooser", autoChooser);
     configureBindings();
   }
 
-  private void configureBindings() {}
+private void configureBindings() { 
+    IntakeButton.whileTrue(
+    new SequentialCommandGroup(
+        new InstantCommand(Elavotor.Position.Bottom, elevator),
+        new InstantCommand(IntakePivot.Position.Intake, intakepivot),
+        new InstantCommand(Intake.intakeCoral, intake)
+        )
+    );
+  OuttakeButton.whileTrue(
+    new InstantCommand(Intake.outakeCoral, intake)
+    );
+  Triangle.onTrue(
+    new InstantCommand(Elevator.Poition.Bottom, elevator)
+    );
+  Cross.onTrue(
+    new InstantCommand(Elevator.Poition.Top, elevator)
+    );
+  /* Is there code for algea intake?
+  R2.whileTrue(
+    new InstantCommand()
+    );*/
+  down.whileTrue(
+  new InstantCommand(Climb.lower, climb)
+    );
+  up.whileTrue(
+    new InstantCommand(Climb.raise, climb)
+    );
+}
 
     public Command getAutonomousCommand() {
       return autoChooser.getSelected();
     }
-  }
+   
+}
+
+
