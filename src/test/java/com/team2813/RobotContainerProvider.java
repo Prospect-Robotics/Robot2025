Prospect-Robotics/Robot2025

commit 595c8777c33e413d084e5177bd0f85f36419ce7f

Robot Changes from pres week

src/main/java/com/team2813/RobotContainer.java
@@ -51,6 +51,7 @@ public class RobotContainer {
     this.elevator = new Elevator(shuffleboard);
     this.intakePivot = new IntakePivot(shuffleboard);
     autoChooser = configureAuto(this.drive);
+
     SmartDashboard.putData("Auto Routine", autoChooser);
     drive.setDefaultCommand(
             new DefaultDriveCommand(
@@ -61,16 +62,18 @@ public class RobotContainer {
     sysIdRoutineSelector = new SysIdRoutineSelector(new SubsystemRegistry(Set.of(drive)), RobotContainer::getSysIdRoutines, shuffleboard);
     RobotCommands autoCommands = new RobotCommands(intake, intakePivot, elevator);
     configureBindings(autoCommands);
-    configureAutoCommands();
+    configureAutoCommands(autoCommands);
+
   }
   
   /**
    * Configure PathPlanner named commands
    * @see <a href="https://pathplanner.dev/pplib-named-commands.html">PathPlanner docs</a>
    */
-  private void configureAutoCommands() {
+  private void configureAutoCommands(RobotCommands autoCommands) {
     Time SECONDS_1 = Units.Seconds.of(1);
     Time SECONDS_2 = Units.Seconds.of(2);
+    NamedCommands.registerCommand("score-coral", autoCommands.placeCoral());
     NamedCommands.registerCommand("ScoreL2", new SequentialCommandGroup(
             new ParallelCommandGroup(
                     new LockFunctionCommand(elevator::atPosition, () -> elevator.setSetpoint(Elevator.Position.BOTTOM), elevator).withTimeout(SECONDS_2),
@@ -83,6 +86,7 @@ public class RobotContainer {
                     new InstantCommand(elevator::disable, elevator)
             )
     ));
+
     //TODO: Test L2 position works well for L1. If it doesn't make this not an alias (make an actual command)
     NamedCommands.registerCommand("ScoreL1", NamedCommands.getCommand("ScoreL2"));
     NamedCommands.registerCommand("ScoreL3", new SequentialCommandGroup(
@@ -238,14 +242,11 @@ public class RobotContainer {
             new SequentialCommandGroup(
                     new ParallelCommandGroup(
                             new LockFunctionCommand(elevator::atPosition, () -> elevator.setSetpoint(Elevator.Position.BOTTOM), elevator).withTimeout(Units.Seconds.of(2)),
-                            new LockFunctionCommand(intakePivot::atPosition, () -> intakePivot.setSetpoint(IntakePivot.Rotations.INTAKE), intakePivot).withTimeout(Units.Seconds.of(2))
+                            new LockFunctionCommand(intakePivot::atPosition, () -> intakePivot.setSetpoint(IntakePivot.Rotations.INTAKE), intakePivot).withTimeout(Units.Seconds.of(0.5))
                     ),
                     new InstantCommand(intake::intakeCoral, intake)
             )
     );
-
-    RESET_POSE.onTrue(new InstantCommand(drive::resetPose, drive));
-
     INTAKE_BUTTON.onFalse(new InstantCommand(intake::stopIntakeMotor, intake));
     
     OUTTAKE_BUTTON.onTrue(new InstantCommand(intake::outakeCoral, intake));

src/main/java/com/team2813/subsystems/Climb.java
@@ -8,6 +8,8 @@ import com.team2813.lib2813.control.InvertType;
 import com.team2813.lib2813.control.PIDMotor;
 import com.team2813.lib2813.control.motors.TalonFXWrapper;
 import com.team2813.lib2813.util.ConfigUtils;
+import edu.wpi.first.wpilibj.DigitalInput;
+
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -20,8 +22,10 @@ import static com.team2813.Constants.*;
 public class Climb extends SubsystemBase{
 
     private final PIDMotor climbMotor1;
+	private DigitalInput limitSwitch;
 
     public Climb() {
+		limitSwitch = new DigitalInput(0);
         TalonFXWrapper climbMotor1 = new TalonFXWrapper(CLIMB_1, InvertType.CLOCKWISE);
 				climbMotor1.motor().setNeutralMode(NeutralModeValue.Brake);
 				TalonFXConfigurator cnf = climbMotor1.motor().getConfigurator();
@@ -38,7 +42,11 @@ public class Climb extends SubsystemBase{
 				climbMotor1.addFollower(CLIMB_2, InvertType.FOLLOW_MASTER);
     }
     public void raise () {
-		climbMotor1.set(ControlMode.VOLTAGE, -8);
+		if (!limitSwitch.get()) {
+            climbMotor1.set(ControlMode.VOLTAGE, -8);
+        } else {
+			climbMotor1.set(ControlMode.VOLTAGE, 0);
+		}
 	}
 	public void lower() {
 		climbMotor1.set(ControlMode.VOLTAGE, 8);
@@ -47,5 +55,12 @@ public class Climb extends SubsystemBase{
 	public void stop() {
 		climbMotor1.set(ControlMode.VOLTAGE, 0);
 	}
+	@Override
+	public void periodic() {
+		if (limitSwitch.get()){
+			climbMotor1.set(ControlMode.VOLTAGE, 0);
+		}
+	}
    
 }
+

src/main/deploy/pathplanner/autos/s1corall2.auto
@@ -1,0 +1,25 @@
+{
+  "version": "2025.0",
+  "command": {
+    "type": "sequential",
+    "data": {
+      "commands": [
+        {
+          "type": "path",
+          "data": {
+            "pathName": "s1corall2"
+          }
+        },
+        {
+          "type": "named",
+          "data": {
+            "name": "score-coral"
+          }
+        }
+      ]
+    }
+  },
+  "resetOdom": true,
+  "folder": null,
+  "choreoAuto": false
+}

src/main/deploy/pathplanner/autos/sideways test.auto
@@ -1,0 +1,19 @@
+{
+  "version": "2025.0",
+  "command": {
+    "type": "sequential",
+    "data": {
+      "commands": [
+        {
+          "type": "path",
+          "data": {
+            "pathName": "sideways test"
+          }
+        }
+      ]
+    }
+  },
+  "resetOdom": true,
+  "folder": null,
+  "choreoAuto": false
+}

src/main/deploy/pathplanner/paths/Copy of s1corall2.path
@@ -1,0 +1,54 @@
+{
+  "version": "2025.0",
+  "waypoints": [
+    {
+      "anchor": {
+        "x": 7.201662465546217,
+        "y": 7.56072039770193
+      },
+      "prevControl": null,
+      "nextControl": {
+        "x": 6.04563960674992,
+        "y": 7.814204555688042
+      },
+      "isLocked": false,
+      "linkedName": null
+    },
+    {
+      "anchor": {
+        "x": 5.468058503839883,
+        "y": 5.3429021034226265
+      },
+      "prevControl": {
+        "x": 4.760439816036739,
+        "y": 5.845520679370777
+      },
+      "nextControl": null,
+      "isLocked": false,
+      "linkedName": null
+    }
+  ],
+  "rotationTargets": [],
+  "constraintZones": [],
+  "pointTowardsZones": [],
+  "eventMarkers": [],
+  "globalConstraints": {
+    "maxVelocity": 3.0,
+    "maxAcceleration": 3.0,
+    "maxAngularVelocity": 540.0,
+    "maxAngularAcceleration": 720.0,
+    "nominalVoltage": 12.0,
+    "unlimited": false
+  },
+  "goalEndState": {
+    "velocity": 0,
+    "rotation": -121.15107652027662
+  },
+  "reversed": false,
+  "folder": null,
+  "idealStartingState": {
+    "velocity": 0,
+    "rotation": -179.95589950626024
+  },
+  "useDefaultConstraints": true
+}

src/main/deploy/pathplanner/paths/New New New New New Path.path
@@ -1,0 +1,54 @@
+{
+  "version": "2025.0",
+  "waypoints": [
+    {
+      "anchor": {
+        "x": 7.923530815972222,
+        "y": 4.993806785300926
+      },
+      "prevControl": null,
+      "nextControl": {
+        "x": 8.923530815972219,
+        "y": 4.993806785300926
+      },
+      "isLocked": false,
+      "linkedName": null
+    },
+    {
+      "anchor": {
+        "x": 6.10733109085648,
+        "y": 4.095810185185185
+      },
+      "prevControl": {
+        "x": 5.10733109085648,
+        "y": 4.095810185185185
+      },
+      "nextControl": null,
+      "isLocked": false,
+      "linkedName": null
+    }
+  ],
+  "rotationTargets": [],
+  "constraintZones": [],
+  "pointTowardsZones": [],
+  "eventMarkers": [],
+  "globalConstraints": {
+    "maxVelocity": 3.0,
+    "maxAcceleration": 3.0,
+    "maxAngularVelocity": 540.0,
+    "maxAngularAcceleration": 720.0,
+    "nominalVoltage": 12.0,
+    "unlimited": false
+  },
+  "goalEndState": {
+    "velocity": 0,
+    "rotation": 0.0
+  },
+  "reversed": false,
+  "folder": null,
+  "idealStartingState": {
+    "velocity": 0,
+    "rotation": 0.0
+  },
+  "useDefaultConstraints": true
+}

src/main/deploy/pathplanner/paths/New New New New Path.path
@@ -1,0 +1,54 @@
+{
+  "version": "2025.0",
+  "waypoints": [
+    {
+      "anchor": {
+        "x": 7.105777994791667,
+        "y": 4.0282609953703705
+      },
+      "prevControl": null,
+      "nextControl": {
+        "x": 8.105777994791673,
+        "y": 4.0282609953703705
+      },
+      "isLocked": false,
+      "linkedName": null
+    },
+    {
+      "anchor": {
+        "x": 4.237150245949073,
+        "y": 6.467660228587963
+      },
+      "prevControl": {
+        "x": 3.2371502459490733,
+        "y": 6.467660228587963
+      },
+      "nextControl": null,
+      "isLocked": false,
+      "linkedName": null
+    }
+  ],
+  "rotationTargets": [],
+  "constraintZones": [],
+  "pointTowardsZones": [],
+  "eventMarkers": [],
+  "globalConstraints": {
+    "maxVelocity": 3.0,
+    "maxAcceleration": 3.0,
+    "maxAngularVelocity": 540.0,
+    "maxAngularAcceleration": 720.0,
+    "nominalVoltage": 12.0,
+    "unlimited": false
+  },
+  "goalEndState": {
+    "velocity": 0,
+    "rotation": 0.0
+  },
+  "reversed": false,
+  "folder": null,
+  "idealStartingState": {
+    "velocity": 0,
+    "rotation": -179.47574548365998
+  },
+  "useDefaultConstraints": true
+}

src/main/deploy/pathplanner/paths/s1corall2.path
@@ -1,0 +1,54 @@
+{
+  "version": "2025.0",
+  "waypoints": [
+    {
+      "anchor": {
+        "x": 7.201662465546217,
+        "y": 7.56072039770193
+      },
+      "prevControl": null,
+      "nextControl": {
+        "x": 6.04563960674992,
+        "y": 7.814204555688042
+      },
+      "isLocked": false,
+      "linkedName": null
+    },
+    {
+      "anchor": {
+        "x": 5.468058503839883,
+        "y": 5.3429021034226265
+      },
+      "prevControl": {
+        "x": 4.748338524070946,
+        "y": 5.8717497954497455
+      },
+      "nextControl": null,
+      "isLocked": false,
+      "linkedName": null
+    }
+  ],
+  "rotationTargets": [],
+  "constraintZones": [],
+  "pointTowardsZones": [],
+  "eventMarkers": [],
+  "globalConstraints": {
+    "maxVelocity": 3.0,
+    "maxAcceleration": 3.0,
+    "maxAngularVelocity": 540.0,
+    "maxAngularAcceleration": 720.0,
+    "nominalVoltage": 12.0,
+    "unlimited": false
+  },
+  "goalEndState": {
+    "velocity": 0,
+    "rotation": -121.15107652027662
+  },
+  "reversed": false,
+  "folder": null,
+  "idealStartingState": {
+    "velocity": 0,
+    "rotation": -179.95589950626024
+  },
+  "useDefaultConstraints": true
+}

commit 499fbc20485cbe1740027c90b0a5b4a1e051bf89

Fix RobotContainerTest and NamedCommandTest
These tests were broken by 595c877.

- Update Climb to implement AutoCloseable and to call limitSwitch.close()
  in close()
- Introduce RobotContainerProvider Rule (which ensures that RobotContainer
  is closed after each test)
- Update RobotContainerTest to use try-with-resources
- Update NamedCommandTest to use RobotContainerProvider

Also:
- Simplifed parameter handling in NamedCommandTest

src/main/java/com/team2813/RobotContainer.java
@@ -34,7 +34,7 @@ import java.util.Set;
 import static com.team2813.Constants.DriverConstants.*;
 import static com.team2813.Constants.OperatorConstants.*;
 
-public class RobotContainer {
+public class RobotContainer implements AutoCloseable {
   private static final DriverStation.Alliance ALLIANCE_USED_IN_PATHS = DriverStation.Alliance.Blue;
   
   private final Climb climb = new Climb();
@@ -290,5 +290,9 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     return autoChooser.getSelected();
   }
-}
 
+  @Override
+  public void close() {
+    climb.close();
+  }
+}

src/main/java/com/team2813/subsystems/Climb.java
@@ -19,7 +19,7 @@ import static com.team2813.Constants.*;
 * Please be kind to her and say hi.
 * Have a nice day!
 */
-public class Climb extends SubsystemBase{
+public class Climb extends SubsystemBase implements AutoCloseable {
 
     private final PIDMotor climbMotor1;
 	private DigitalInput limitSwitch;
@@ -61,6 +61,9 @@ public class Climb extends SubsystemBase{
 			climbMotor1.set(ControlMode.VOLTAGE, 0);
 		}
 	}
-   
-}
 
+    @Override
+    public void close() {
+        limitSwitch.close();
+    }
+}

src/test/java/com/team2813/NamedCommandTest.java
@@ -18,22 +18,12 @@ import static org.junit.Assume.assumeNotNull;
 
 @RunWith(Parameterized.class)
 public class NamedCommandTest {
-  @Parameters
-  public static Collection<Object[]> data() {
-    return Arrays.asList(new Object[][] {
-            {"ScoreL1"}, {"ScoreL2"}, {"ScoreL3"}, {"BumpAlgaeLow"}, {"BumpAlgaeHigh"}, {"IntakeCoral"}
-    });
-  }
-  private final FakeShuffleboardTabs shuffleboard = new FakeShuffleboardTabs();
-  private RobotContainer robotContainer;
-  
-  @Before
-  public void startRobotContainer() {
-    try {
-      robotContainer = new RobotContainer(shuffleboard);
-    } catch (Exception e) {
-      // Don't need to do anything
-    }
+  @Rule
+  public final RobotContainerProvider robotContainerProvider = new RobotContainerProvider();
+
+  @Parameters(name = "{0}")
+  public static Collection<?> data() {
+    return Arrays.asList("ScoreL1", "ScoreL2", "ScoreL3", "BumpAlgaeLow", "BumpAlgaeHigh", "IntakeCoral");
   }
   
   @Parameter(0)
@@ -41,7 +31,8 @@ public class NamedCommandTest {
   
   @Test
   public void commandExists() {
-    assumeNotNull(commandName, robotContainer);
+    var robotContainer = robotContainerProvider.get();
+
     assertThat(NamedCommands.hasCommand(commandName)).isTrue();
   }
 }

src/test/java/com/team2813/RobotContainerProvider.java
@@ -1,0 +1,40 @@
+package com.team2813;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+
+import org.junit.rules.ExternalResource;
+
+import java.util.function.Supplier;
+
+public final class RobotContainerProvider extends ExternalResource  {
+  private final Supplier<RobotContainer> supplier;
+  private RobotContainer robotContainer = null;
+
+  public RobotContainerProvider() {
+    this(() -> new RobotContainer(new FakeShuffleboardTabs()));
+  }
+
+  public RobotContainerProvider(Supplier<RobotContainer> supplier) {
+    this.supplier = supplier;
+  }
+
+  public RobotContainer get() {
+    if (robotContainer == null) {
+      throw new IllegalStateException("Cannot call get() until the RobotContainerProvider @Rule is run");
+    }
+    return robotContainer;
+  }
+
+  @Override
+  protected void before() throws Throwable {
+    assertWithMessage("before() cannot be called twice").that(robotContainer).isNull();;
+    robotContainer = this.supplier.get();
+  }
+
+  @Override
+  protected void after() {
+    if (robotContainer != null) {
+      robotContainer.close();
+    }
+  }
+}

src/test/java/com/team2813/RobotContainerTest.java
@@ -1,5 +1,7 @@
 package com.team2813;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -10,11 +12,13 @@ public final class RobotContainerTest {
 
     @Test
     public void constructorDoesNotRaise() {
-        new RobotContainer(shuffleboard);
+        //noinspection EmptyTryBlock
+        try (var container = new RobotContainer(shuffleboard)) {}
     }
 
     @Test
     public void conBeConstructedMultipleTimes() {
-        new RobotContainer(shuffleboard);
+        //noinspection EmptyTryBlock
+        try (var container = new RobotContainer(shuffleboard)) {}
     }
 }