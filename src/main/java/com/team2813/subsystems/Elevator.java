96b39a1c4f6217666907cbf155c42e0d8e2b1355
cuttestkittensrule <mangoiscute95@gmail.com>
2025-03-17 16:21:39 -0700

    Fix the elevator positioning... again (#102)
    * copy over top and bottom positions
    
    * move the positions slightly closer to the center to prevent slamming into the bottom

--- src/main/java/com/team2813/subsystems/Elevator.java
+++ src/main/java/com/team2813/subsystems/Elevator.java
@@ -56,9 +56,8 @@ public class Elevator extends MotorSubsystem<Elevator.Position> {
   }
 
   public enum Position implements Supplier<Angle> {
-    BOTTOM(0.321777),
-    TEST(10),
-    TOP(25.972656);
+    BOTTOM(-0.285742),
+    TOP(25.355566);
 
     private final Angle position;
 
