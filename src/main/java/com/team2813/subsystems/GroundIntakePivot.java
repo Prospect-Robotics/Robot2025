048cf5c9a82f7c17bfc653cb159af54e00265c6c
spderman3333 <118777573+spderman3333@users.noreply.github.com>
2025-03-17 16:28:36 -0700

    Implemented the GroundIntakePivot, will likely need lots of tuning

--- src/main/java/com/team2813/subsystems/GroundIntakePivot.java
+++ src/main/java/com/team2813/subsystems/GroundIntakePivot.java
@@ -1,4 +1,86 @@
 package com.team2813.subsystems;
 
-public class GroundIntakePivot {
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.team2813.lib2813.control.ControlMode;
+import com.team2813.lib2813.control.InvertType;
+import com.team2813.lib2813.control.PIDMotor;
+import com.team2813.lib2813.control.motors.TalonFXWrapper;
+import com.team2813.lib2813.subsystems.MotorSubsystem;
+import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+
+import java.util.function.Supplier;
+
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.Rotations;
+
+//TODO: TUNE ALL THE THINGS!!1! AND ALSO SET UP THE POSITIONS ENUM PROPERLY.
+
+public class GroundIntakePivot extends MotorSubsystem<GroundIntakePivot.Positions> {
+
+    private final DoublePublisher groundIntakePivot;
+    private final BooleanPublisher atPosition;
+
+    public GroundIntakePivot(NetworkTableInstance networkTableInstance) {
+        super(new MotorSubsystemConfiguration(getMotor())
+                .controlMode(ControlMode.VOLTAGE)
+                .PID(0, 0, 0) //TODO: Configure the PID. Maybe I should try this on my own.
+                .rotationUnit(Radians)
+                .acceptableError(1.0) //TODO: Tune this as well.
+        );
+
+        NetworkTable networkTable = networkTableInstance.getTable("GroundIntakePivot");
+        groundIntakePivot = networkTable.getDoubleTopic("position").publish();
+        atPosition = networkTable.getBooleanTopic("at position").publish();
+    }
+
+    private static TalonFXWrapper getMotor() {
+        TalonFXWrapper wrapper = new TalonFXWrapper(22, InvertType.CLOCKWISE); // +x = Up, -x = Down.
+        wrapper.setNeutralMode(NeutralModeValue.Brake);
+
+        return wrapper;
+    }
+
+    @Override
+    protected void useOutput(double output, double setPoint) { //NOTE: setPoint seems to do nothing in MotorSubsystem.java, I could be wrong though
+        super.useOutput(output, setPoint);
+    }
+
+    private static PIDMotor pivotMotor() {
+        TalonFXWrapper pivotMotor =
+                new TalonFXWrapper(com.team2813.Constants.INTAKE_PIVOT, InvertType.COUNTER_CLOCKWISE);
+        pivotMotor.setNeutralMode(NeutralModeValue.Brake);
+
+        return pivotMotor;
+    }
+
+    @Override
+    public void periodic() {
+        super.periodic();
+        groundIntakePivot.set(getPositionMeasure().in(Units.Rotations));
+        atPosition.set(atPosition());
+    }
+
+
+    public enum Positions implements Supplier<Angle> {
+        BOTTOM(0.0), // FIXME: SET THESE UP TO THE PROPER POSITIONS.
+        TEST(5),
+        TOP(0.0); // FIXME: SET THESE UP TO THE PROPER POSITIONS.
+
+        private final Angle position;
+
+        Positions(double position) {
+            this.position = Rotations.of(position);
+        }
+
+        @Override
+        public Angle get() {
+            return position;
+        }
+    }
+
 }
