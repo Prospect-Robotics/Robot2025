d15db7ed70efb9cfbcc343fb559d53586bf6c758
Stefan Dikov <118777573+spderman3333@users.noreply.github.com>
2025-03-19 20:36:28 -0700

    Removed test position on Positions ENUM

--- src/main/java/com/team2813/subsystems/GroundIntakePivot.java
+++ src/main/java/com/team2813/subsystems/GroundIntakePivot.java
@@ -61,9 +61,8 @@ public class GroundIntakePivot extends MotorSubsystem<GroundIntakePivot.Position
   }
 
   public enum Positions implements Supplier<Angle> {
-    BOTTOM(-6.497070), // FIXME: SET THESE UP TO THE PROPER POSITIONS.
-    TEST(-5),
-    TOP(0.773438); // FIXME: TEST THIS!
+    BOTTOM(-6.497070),
+    TOP(0.773438);
 
     private final Angle position;
 
