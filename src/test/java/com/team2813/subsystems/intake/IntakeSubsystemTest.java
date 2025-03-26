1b3e1b88db3174c1e1ae86245e854a8504cfd54b
Kevin Cooney <kcooney@gmail.com>
2025-03-26 10:42:53 -0700

    Update IntakeSubsystemTest to use WPILibExtension

--- src/test/java/com/team2813/subsystems/intake/IntakeSubsystemTest.java
+++ src/test/java/com/team2813/subsystems/intake/IntakeSubsystemTest.java
@@ -7,13 +7,16 @@ import static org.mockito.Mockito.verifyNoInteractions;
 import com.team2813.IsolatedNetworkTablesExtension;
 import com.team2813.lib2813.control.ControlMode;
 import com.team2813.lib2813.control.PIDMotor;
+import com.team2813.lib2813.testing.junit.jupiter.CommandTester;
+import com.team2813.lib2813.testing.junit.jupiter.WPILibExtension;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Answers;
 
-@ExtendWith(IsolatedNetworkTablesExtension.class)
+@ExtendWith({IsolatedNetworkTablesExtension.class, WPILibExtension.class})
 public final class IntakeSubsystemTest {
   final FakePIDMotor fakeMotor = mock(FakePIDMotor.class, Answers.CALLS_REAL_METHODS);
   final DigitalInput mockBeamBreak = mock(DigitalInput.class);
@@ -44,62 +47,86 @@ public final class IntakeSubsystemTest {
   }
 
   @Test
-  public void intakeCoral(NetworkTableInstance ntInstance) {
-    try (IntakeSubsystem intake = new IntakeSubsystem(fakeMotor, mockBeamBreak, ntInstance)) {
-      intake.intakeCoral();
+  public void intakeCoral(NetworkTableInstance ntInstance, CommandTester CommandTesterInterface) {
+    try (var intake = new IntakeSubsystem(fakeMotor, mockBeamBreak, ntInstance)) {
+      Command command = intake.intakeItemCommand();
+      assertThat(fakeMotor.dutyCycle).isWithin(0.01).of(0.0);
+
+      CommandTesterInterface.runUntilComplete(command);
 
       assertThat(fakeMotor.dutyCycle).isWithin(0.01).of(IntakeSubsystem.PARAMS.intakeDemand());
     }
   }
 
   @Test
-  public void stopAfterIntakingCoral(NetworkTableInstance ntInstance) {
-    try (IntakeSubsystem intake = new IntakeSubsystem(fakeMotor, mockBeamBreak, ntInstance)) {
-      intake.intakeCoral();
+  public void stopAfterIntakingCoral(
+      NetworkTableInstance ntInstance, CommandTester CommandTesterInterface) {
+    try (var intake = new IntakeSubsystem(fakeMotor, mockBeamBreak, ntInstance)) {
+      Command command = intake.intakeItemCommand();
+      CommandTesterInterface.runUntilComplete(command);
+      command = intake.stopMotorCommand();
+      assertThat(fakeMotor.dutyCycle).isGreaterThan(0.01);
 
-      intake.stopMotor();
+      CommandTesterInterface.runUntilComplete(command);
 
       assertThat(fakeMotor.dutyCycle).isWithin(0.01).of(0.0);
     }
   }
 
   @Test
-  public void outtakeCoral(NetworkTableInstance ntInstance) {
-    try (IntakeSubsystem intake = new IntakeSubsystem(fakeMotor, mockBeamBreak, ntInstance)) {
-      intake.outtakeCoral();
+  public void outtakeCoral(NetworkTableInstance ntInstance, CommandTester CommandTesterInterface) {
+    try (var intake = new IntakeSubsystem(fakeMotor, mockBeamBreak, ntInstance)) {
+      intake.intakeCoral();
+      Command command = intake.outtakeItemCommand();
+      assertThat(fakeMotor.dutyCycle).isGreaterThan(0.01);
+
+      CommandTesterInterface.runUntilComplete(command);
 
       assertThat(fakeMotor.dutyCycle).isWithin(0.01).of(IntakeSubsystem.PARAMS.outtakeDemand());
     }
   }
 
   @Test
-  public void stopAfterOuttakingCoral(NetworkTableInstance ntInstance) {
-    try (IntakeSubsystem intake = new IntakeSubsystem(fakeMotor, mockBeamBreak, ntInstance)) {
-      intake.outtakeCoral();
+  public void stopAfterOuttakingCoral(
+      NetworkTableInstance ntInstance, CommandTester CommandTesterInterface) {
+    try (var intake = new IntakeSubsystem(fakeMotor, mockBeamBreak, ntInstance)) {
+      intake.intakeCoral();
+      Command command = intake.outtakeItemCommand();
+      CommandTesterInterface.runUntilComplete(command);
+      command = intake.stopMotorCommand();
 
-      intake.stopMotor();
+      CommandTesterInterface.runUntilComplete(command);
 
       assertThat(fakeMotor.dutyCycle).isWithin(0.01).of(0.0);
+      assertThat(fakeMotor.dutyCycle).isWithin(0.01).of(0);
     }
   }
 
   @Test
-  public void bumpAlgae(NetworkTableInstance ntInstance) {
-    try (IntakeSubsystem intake = new IntakeSubsystem(fakeMotor, mockBeamBreak, ntInstance)) {
-      intake.bumpAlgae();
+  public void bumpAlgae(NetworkTableInstance ntInstance, CommandTester CommandTesterInterface) {
+    try (var intake = new IntakeSubsystem(fakeMotor, mockBeamBreak, ntInstance)) {
+      intake.intakeCoral();
+      Command command = intake.bumpAlgaeCommand();
+      assertThat(fakeMotor.dutyCycle).isGreaterThan(0.01);
+
+      CommandTesterInterface.runUntilComplete(command);
 
       assertThat(fakeMotor.dutyCycle).isWithin(0.01).of(IntakeSubsystem.BUMP_VOLTAGE);
     }
   }
 
   @Test
-  public void stopAfterBumpingAlgae(NetworkTableInstance ntInstance) {
-    try (IntakeSubsystem intake = new IntakeSubsystem(fakeMotor, mockBeamBreak, ntInstance)) {
-      intake.bumpAlgae();
+  public void stopAfterBumpingAlgae(
+      NetworkTableInstance ntInstance, CommandTester CommandTesterInterface) {
+    try (var intake = new IntakeSubsystem(fakeMotor, mockBeamBreak, ntInstance)) {
+      Command command = intake.bumpAlgaeCommand();
+      CommandTesterInterface.runUntilComplete(command);
+      command = intake.stopMotorCommand();
 
-      intake.stopMotor();
+      CommandTesterInterface.runUntilComplete(command);
 
       assertThat(fakeMotor.dutyCycle).isWithin(0.01).of(0.0);
+      assertThat(fakeMotor.dutyCycle).isWithin(0.01).of(0);
     }
   }
 }
