263dc0c4cb604e9fa11faed9f56fdf60cc058d74
Kevin Cooney <kcooney@users.noreply.github.com>
2025-04-03 22:53:36 -0600

    Add Javadoc to clarify behavior of location-based APIs (#108)

--- src/main/java/com/team2813/commands/RobotLocalization.java
+++ src/main/java/com/team2813/commands/RobotLocalization.java
@@ -98,7 +98,13 @@ public class RobotLocalization { // TODO: consider making this a subsystem so we
   private final StructPublisher<Pose2d> lastPosePublisher =
       NetworkTableInstance.getDefault().getStructTopic("Auto Align to", Pose2d.struct).publish();
 
-  private Command createPath(Supplier<Pose2d> drivePosSupplier, List<Pose2d> positions) {
+  /**
+   * Creates a command from the current position to the nearest target.
+   *
+   * @param drivePosSupplier Provides the current robot position (in the blue coordinate system).
+   * @param targets Locations to place coral, one for each of the sides of the coral reef.
+   */
+  private Command createPath(Supplier<Pose2d> drivePosSupplier, List<Pose2d> targets) {
     Pose2d currentPose = drivePosSupplier.get();
     if (Drive.onRed()) {
       currentPose =
@@ -108,7 +114,7 @@ public class RobotLocalization { // TODO: consider making this a subsystem so we
               currentPose.getRotation().plus(new Rotation2d(Math.PI)));
     }
 
-    Pose2d newPosition = currentPose.nearest(positions);
+    Pose2d newPosition = currentPose.nearest(targets);
     lastPosePublisher.set(newPosition);
 
     List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(currentPose, newPosition);
