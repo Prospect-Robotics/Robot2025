aa09f6b0b38d0a70c1aa1656d3559fa4376b9077
CXLYSG <115902987+CXLYSG@users.noreply.github.com>
2025-04-07 15:44:39 -0700

    ground intake setpoint changes

--- src/main/java/com/team2813/subsystems/GroundIntake.java
+++ src/main/java/com/team2813/subsystems/GroundIntake.java
@@ -22,7 +22,7 @@ public class GroundIntake extends SubsystemBase {
   private boolean isActive = false;
 
   private static final double INTAKE_SPEED = 8; // TODO: Tweak speed
-  private static final double OUTTAKE_SPEED = -8; // TODO: Tweak speed
+  private static final double OUTTAKE_SPEED = -3.2; // TODO: Tweak speed
 
   private static final AngularVelocity STALL_SPEED =
       RotationsPerSecond.of(
