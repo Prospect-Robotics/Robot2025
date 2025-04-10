15c6b0d5616007dcdbc98acf7d96223fce66a8c3
Kevin Cooney <kcooney@gmail.com>
2025-04-10 12:20:14 -0700

    Publish description for PhotonVison cameras to network tables

--- src/main/java/com/team2813/vision/LimelightPosePublisher.java
+++ src/main/java/com/team2813/vision/LimelightPosePublisher.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 import java.util.function.Supplier;
 
 public final class LimelightPosePublisher {
-  private static final String CAMERA_NAME = "limelight";
+  static final String CAMERA_NAME = "limelight";
   private static final long MICROS_PER_SECOND = 1_000_000;
   private final TimestampedStructPublisher<Pose2d> publisher;
   private final double timestampOffset;
