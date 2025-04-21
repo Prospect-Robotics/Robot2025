41c7b822c6848b2ece0a720b4a2c5691d9024eb1
cuttestkittensrule <mangoiscute95@gmail.com>
2025-04-21 01:11:25 -0500

    almost finish DriverStationSim modification builder

--- src/test/java/com/team2813/DriverStationSimResource.java
+++ src/test/java/com/team2813/DriverStationSimResource.java
@@ -1,17 +1,15 @@
 package com.team2813;
 
-import com.google.auto.value.AutoBuilder;
 import edu.wpi.first.hal.AllianceStationID;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
-import org.junit.rules.ExternalResource;
-
 import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.OptionalInt;
+import org.junit.rules.ExternalResource;
 
 public class DriverStationSimResource extends ExternalResource {
-  
+
   public static class ModificationBuilder {
     private Optional<Boolean> enabled = Optional.empty();
     private Optional<Boolean> autonomous = Optional.empty();
@@ -27,55 +25,78 @@ public class DriverStationSimResource extends ExternalResource {
     private Optional<DriverStation.MatchType> matchType = Optional.empty();
     private OptionalInt matchNumber = OptionalInt.empty();
     private OptionalInt replayNumber = OptionalInt.empty();
-    
+
     public ModificationBuilder enabled(boolean enabled) {
       this.enabled = Optional.of(enabled);
       return this;
     }
-    
+
     public ModificationBuilder autonomous(boolean autonomous) {
       this.autonomous = Optional.of(autonomous);
       return this;
     }
-    
+
     public ModificationBuilder test(boolean test) {
       this.test = Optional.of(test);
       return this;
     }
-    
+
     public ModificationBuilder eStop(boolean eStop) {
       this.eStop = Optional.of(eStop);
       return this;
     }
-    
+
     public ModificationBuilder fmsAttached(boolean fmsAttached) {
       this.fmsAttached = Optional.of(fmsAttached);
       return this;
     }
-    
+
     public ModificationBuilder dsAttahced(boolean dsAttahced) {
       this.dsAttached = Optional.of(dsAttahced);
       return this;
     }
-    
+
     public ModificationBuilder allianceStationId(AllianceStationID allianceStationId) {
       this.allianceStationId = Optional.of(allianceStationId);
       return this;
     }
-    
+
     public ModificationBuilder matchTime(double matchTime) {
       this.matchTime = OptionalDouble.of(matchTime);
       return this;
     }
-    
+
     public ModificationBuilder sendError(boolean sendError) {
       this.sendError = Optional.of(sendError);
       return this;
     }
-    
-    public ModificationBuilder sendConsoleLine(boolean)
+
+    public ModificationBuilder sendConsoleLine(boolean sendConsoleLine) {
+      this.sendConsoleLine = Optional.of(sendConsoleLine);
+      return this;
+    }
+
+    public ModificationBuilder eventName(String eventName) {
+      this.eventName = Optional.of(eventName);
+      return this;
+    }
+
+    public ModificationBuilder matchType(DriverStation.MatchType matchType) {
+      this.matchType = Optional.of(matchType);
+      return this;
+    }
+
+    public ModificationBuilder matchNumber(int matchNumber) {
+      this.matchNumber = OptionalInt.of(matchNumber);
+      return this;
+    }
+
+    public ModificationBuilder replayNumber(int replayNumber) {
+      this.replayNumber = OptionalInt.of(replayNumber);
+      return this;
+    }
   }
-  
+
   private void performModification(ModificationBuilder modification) {
     modification.enabled.ifPresent(DriverStationSim::setEnabled);
     modification.autonomous.ifPresent(DriverStationSim::setAutonomous);
@@ -85,24 +106,12 @@ public class DriverStationSimResource extends ExternalResource {
     modification.dsAttached.ifPresent(DriverStationSim::setDsAttached);
     modification.allianceStationId.ifPresent(DriverStationSim::setAllianceStationId);
     modification.matchTime.ifPresent(DriverStationSim::setMatchTime);
+    modification.sendError.ifPresent(DriverStationSim::setSendError);
+    modification.sendConsoleLine.ifPresent(DriverStationSim::setSendConsoleLine);
+    modification.eventName.ifPresent(DriverStationSim::setEventName);
     modification.matchType.ifPresent(DriverStationSim::setMatchType);
     modification.matchNumber.ifPresent(DriverStationSim::setMatchNumber);
     modification.replayNumber.ifPresent(DriverStationSim::setMatchNumber);
-    DriverStationSim.notifyNewData();
-  }
-
-  private void performModification(Modification modification) {
-    DriverStationSim.setEnabled(modification.enabled);
-    DriverStationSim.setAutonomous(modification.autonomous);
-    DriverStationSim.setTest(modification.test);
-    DriverStationSim.setEStop(modification.eStop);
-    DriverStationSim.setFmsAttached(modification.fmsAttached);
-    DriverStationSim.setDsAttached(modification.dsAttached);
-    DriverStationSim.setAllianceStationId(modification.allianceStationID);
-    DriverStationSim.setMatchTime(modification.matchTime);
-    DriverStationSim.setMatchType(modification.matchType);
-    DriverStationSim.setMatchNumber(modification.matchNumber);
-    DriverStationSim.setMatchNumber(modification.replayNumber);
     DriverStationSim.notifyNewData();
   }
 
