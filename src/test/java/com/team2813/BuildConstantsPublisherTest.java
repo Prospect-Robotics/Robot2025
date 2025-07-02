9eda8b2aa0cf8916b7fb3c781c2b08d9009d9653
Kevin Cooney <kcooney@gmail.com>
2025-07-02 16:16:14 -0700

    Support JUnit Jupiter tests

--- src/test/java/com/team2813/BuildConstantsPublisherTest.java
+++ src/test/java/com/team2813/BuildConstantsPublisherTest.java
@@ -14,7 +14,7 @@ import java.io.PrintStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class BuildConstantsPublisherTest {
 
