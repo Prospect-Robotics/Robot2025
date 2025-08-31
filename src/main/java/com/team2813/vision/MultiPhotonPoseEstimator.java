e49150830ea101ae3d049ee58731d3c652f61006
vdikov <120233643+vdikov@users.noreply.github.com>
2025-08-30 18:34:29 -0700

    Add documentation to MultiPhotonPoseEstimator (#157)
    * Attempt to document MultiPhotonPoseEstimator.java
    
    * Add more comments related to the multi-photon pose estimator class
    
    * Minor comment touch-ups
    
    * Rebased to current main
    
    * Dedup functions duplicated during merge
    
    * Reset lib2813 to where it is still consistent with the main code. lib2813 is currently ahead of the Robot2025 code
    
    * Addressed most reviewer comments or left/elaborated todos to capture some reviewer ideas for future reference

--- src/main/java/com/team2813/vision/MultiPhotonPoseEstimator.java
+++ src/main/java/com/team2813/vision/MultiPhotonPoseEstimator.java
@@ -5,7 +5,11 @@ import static com.team2813.vision.VisionNetworkTables.CAMERA_POSE_TOPIC;
 import static com.team2813.vision.VisionNetworkTables.getTableForCamera;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
@@ -23,6 +27,23 @@ import org.photonvision.simulation.PhotonCameraSim;
 import org.photonvision.simulation.SimCameraProperties;
 import org.photonvision.simulation.VisionSystemSim;
 
+/**
+ * Provides estimated robot positions, in field pose, from multiple PhotonVision cameras.
+ *
+ * <p>This class manages one or more PhotonVision cameras, and provides an API {@link
+ * #update(Consumer)} to provide an updated estimated robot pose by combining readings from
+ * AprilTags visible from the cameras. It also supports adding camera configurations to
+ * PhotonVision's simulated vision system.
+ *
+ * <p>Note that, when we are dealing with 2D and 3D poses, the we follow the transformation
+ * conventions established by WPILib and PhotonVision:
+ * https://docs.photonvision.org/en/latest/docs/apriltag-pipelines/coordinate-systems.html
+ *
+ * <p>Furthermore note that the global robot pose or any of the camera global poses are also
+ * referred to as "field-centric pose". In our librarires, field-centric poses are always specified
+ * relative to the blue origin per
+ * https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html#always-blue-origin
+ */
 public class MultiPhotonPoseEstimator implements AutoCloseable {
   private final List<CameraData> cameraDatas = new ArrayList<>();
 
@@ -32,6 +53,15 @@ public class MultiPhotonPoseEstimator implements AutoCloseable {
     private final NetworkTableInstance ntInstance;
     private final PhotonPoseEstimator.PoseStrategy poseStrategy;
 
+    /**
+     * MultiPhotonPoseEstimator builder constructor.
+     *
+     * @param ntInstance Network table instance used to log the pose of AprilTag detections as well
+     *     as pose estimates.
+     * @param fieldTags WPILib field description (dimensions) including AprilTag 3D locations.
+     * @param poseStrategy Posing strategy (for instance, multi tag PnP, closest to camera tag,
+     *     etc.)
+     */
     public Builder(
         NetworkTableInstance ntInstance,
         AprilTagFieldLayout fieldTags,
@@ -41,14 +71,37 @@ public class MultiPhotonPoseEstimator implements AutoCloseable {
       this.poseStrategy = poseStrategy;
     }
 
+    /**
+     * Adds a camera to the multi pose estimator.
+     *
+     * @param name Unique name of the camera.
+     * @param transform 3D position of the camera relative to the robot frame.
+     * @return Builder instance.
+     */
     public Builder addCamera(String name, Transform3d transform) {
       return addCamera(name, transform, Optional.empty());
     }
 
+    /**
+     * Adds a camera to the multi pose estimator.
+     *
+     * @param name Unique name of the camera.
+     * @param transform 3D position of the camera relative to the robot frame.
+     * @param description Camera description.
+     * @return Builder instance.
+     */
     public Builder addCamera(String name, Transform3d transform, String description) {
       return addCamera(name, transform, Optional.of(description));
     }
 
+    /**
+     * Adds a camera to the multi pose estimator.
+     *
+     * @param name Unique name of the camera.
+     * @param transform 3D position of the camera relative to the robot frame.
+     * @param description Camera description.
+     * @return Builder instance.
+     */
     private Builder addCamera(String name, Transform3d transform, Optional<String> description) {
       if (name.equals(LIMELIGHT_CAMERA_NAME)) {
         throw new IllegalArgumentException(String.format("Invalid camera name: '%s'", name));
@@ -60,15 +113,17 @@ public class MultiPhotonPoseEstimator implements AutoCloseable {
       return this;
     }
 
+    /** Builds a configured MultiPhotonPoseEstimator. */
     public MultiPhotonPoseEstimator build() {
       return new MultiPhotonPoseEstimator(this);
     }
   }
 
   /**
-   * Adds the cameras to the provided simulated vision system.
+   * Adds the current Multi-Photon camera setup to a simulated vision system.
    *
-   * @param propertyFactory Called to get the simulated camera properties for each camera.
+   * @param simVisionSystem The simulated visual system.
+   * @param propertyFactory Functor that creates simulated camera properties.
    */
   public void addToSim(
       VisionSystemSim simVisionSystem, Function<String, SimCameraProperties> propertyFactory) {
@@ -80,8 +135,22 @@ public class MultiPhotonPoseEstimator implements AutoCloseable {
         });
   }
 
+  /** A camera that is connected to PhotonVision. */
   private record CameraConfig(Transform3d robotToCamera, Optional<String> description) {}
 
+  /**
+   * Holds configuration for a camera for the Builder.
+   *
+   * @param camera A camera connected to PhotonVision.
+   * @param estimator A pose estimator configured for this camera.
+   * @param robotToCamera The 3D fixed pose of the camera relative to the robot. Intuitively, this
+   *     field describes where on the robot the camera is mounted.
+   * @param publisher A publisher reporting PhotonVision pose detections to NetworkTables during the
+   *     robot runtime.
+   * @param cameraPosePublisher A publisher reporting the position of the camera in field-centric
+   *     coordinates. In other words, this is the pose most recently set by {@link @setDrivePose}
+   *     with the camera's own robotToCamera pose appended to it.
+   */
   private record CameraData(
       PhotonCamera camera,
       PhotonPoseEstimator estimator,
@@ -89,18 +158,33 @@ public class MultiPhotonPoseEstimator implements AutoCloseable {
       PhotonVisionPosePublisher publisher,
       StructPublisher<Pose3d> cameraPosePublisher) {}
 
+  /** Creates an instance using values from a {@code Builder}. */
   private MultiPhotonPoseEstimator(Builder builder) {
-    for (Map.Entry<String, CameraConfig> entry : builder.cameraConfigs.entrySet()) {
+    for (var entry : builder.cameraConfigs.entrySet()) {
       String cameraName = entry.getKey();
-      PhotonCamera camera = new PhotonCamera(builder.ntInstance, cameraName);
       CameraConfig cameraConfig = entry.getValue();
+
+      PhotonCamera camera = new PhotonCamera(builder.ntInstance, cameraName);
       PhotonPoseEstimator estimator =
           new PhotonPoseEstimator(
               builder.fieldTags, builder.poseStrategy, cameraConfig.robotToCamera);
+      // A NetworkTables publisher, preconfigured to post timestamped pose estimates from `camera`.
       var estimatedPosePublisher = new PhotonVisionPosePublisher(camera, builder.fieldTags);
+      // Gets a NetworkTables subtable, dedicated to `camera`.
+      // It will typically be nested under `photonvision/Vision/<camera_name>`
+      // (TODO(vdikov): consider restructuring how the topics under which we publish in network
+      // tables are more explicitly listed somewhere, e.g., in a constants file of sorts. Right now,
+      // the actual path under which we publish is constructed across multiple levels of function
+      // calls, so if a software developer needs to track where a specific value they observe in,
+      // say, Advantage Scope is reported from, they have to trace through all these function
+      // calls. In contrast, the easiest paths to track from one system back to the code are the
+      // hard-coded paths - but that's not really an option here either, since we need dynamic
+      // information, like the camera name, to be part of the final topic path. Using template paths
+      // might be a good middle ground.)
       NetworkTable table = getTableForCamera(camera);
       var cameraPosePublisher = table.getStructTopic(CAMERA_POSE_TOPIC, Pose3d.struct).publish();
 
+      // Add the camera data to camera datas for the lifetime of the multi-photon pose estimator.
       cameraDatas.add(
           new CameraData(
               camera,
@@ -114,6 +198,26 @@ public class MultiPhotonPoseEstimator implements AutoCloseable {
     }
   }
 
+  /**
+   * Sets a 2D pose estimate in a field-centric frame (relative to the blue origin).
+   *
+   * <p>This method takes a field-centric drive train pose (drive train and robot are the same
+   * here), update the camera field-centric poses and publish them on network tables.
+   *
+   * <p>TODO(vdikov): This method sits very counter-intiutively in this class. The class is all
+   * about estimating pose and feeding it to the drive train pose estimation. Yet, this method is
+   * feeding a drive-train `pose` back to it. One could reasonably assume that the drive train pose
+   * is somehow used for the multi-photon pose estimation and plays some role there. But the method
+   * code tells a much more prosaic story - the `pose` is only used for reporting camera poses,
+   * relative to "some" drive-train (at that point we don't even know if that pose is derived from
+   * the multi-photon pose estimation whatsoever). The MultiPhotonPoseEstimator API would become
+   * cleaner if we remove this method and find other ways to report Camera poses. kcooney@ has
+   * drafted several cool ideas how we can address that with a better class/interfaces architecture
+   * here: https://github.com/Prospect-Robotics/Robot2025/pull/157#discussion_r2282753534
+   *
+   * @param pose 2D field-centric (relative to blue origin) pose of the drive train (i.e., the
+   *     robot).
+   */
   public void setDrivePose(Pose2d pose) {
     Pose3d pose3d = new Pose3d(pose);
     for (CameraData cameraData : cameraDatas) {
@@ -171,6 +275,18 @@ public class MultiPhotonPoseEstimator implements AutoCloseable {
     }
   }
 
+  /**
+   * Takes a consumer for estimated poses and applies all unread robot-pose estimatations from all
+   * cameras against `apply`.
+   *
+   * <p>This method is supposed to be called from a routine updating drive-train pose with pose
+   * estimates from the photon vision cameras.
+   *
+   * <p>TODO(vdikov): Further ideas how to refactor this interface are suggested by kcooney@ in this
+   * comment https://github.com/Prospect-Robotics/Robot2025/pull/157#discussion_r2282806711
+   *
+   * @param apply Callback to consume unread photonevision robot-pose estimations.
+   */
   public void update(Consumer<? super EstimatedRobotPose> apply) {
     for (CameraData cameraData : cameraDatas) {
       List<EstimatedRobotPose> poses =
