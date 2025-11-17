6fe7d1246fa45da963961c1e8682b2530653252b
cuttestkittensrule <mangoiscute95@gmail.com>
2025-11-17 15:24:37 -0800

    Add regression test that fails when periodic fails

--- src/test/java/com/team2813/RobotContainerTest.java
+++ src/test/java/com/team2813/RobotContainerTest.java
@@ -1,10 +1,13 @@
 package com.team2813;
 
+import com.team2813.lib2813.testing.junit.jupiter.CommandTester;
+import com.team2813.lib2813.testing.junit.jupiter.WPILibExtension;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.Commands;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(IsolatedNetworkTablesExtension.class)
+@ExtendWith({IsolatedNetworkTablesExtension.class, WPILibExtension.class})
 public final class RobotContainerTest {
   private final FakeShuffleboardTabs shuffleboard = new FakeShuffleboardTabs();
 
@@ -18,5 +21,14 @@ public final class RobotContainerTest {
   public void conBeConstructedMultipleTimes(NetworkTableInstance ntInstance) {
     //noinspection EmptyTryBlock
     try (var container = new RobotContainer(shuffleboard, ntInstance)) {}
+  }
+
+  @Test
+  public void periodicDoesNotFail(CommandTester commandTester, NetworkTableInstance ntInstance) {
+    try (RobotContainer container = new RobotContainer(shuffleboard, ntInstance)) {
+      // Runs the periodic method at least once. If the periodic method would fail with an
+      // exception, this will fail with an exception and fail the test.
+      commandTester.runUntilComplete(Commands.none());
+    }
   }
 }
